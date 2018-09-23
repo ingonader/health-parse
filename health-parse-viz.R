a320643b75f754a611b38e2c9faf6386d10191c3
Ingo Nader <ingo.nader@unbelievable-machine.com>
2018-09-23 10:04:19 +0200

    Remove unnecessary code and comments (todos that were already done)

--- health-parse-viz.R
+++ health-parse-viz.R
@@ -5,63 +5,8 @@
 ## Author Ingo Nader
 ## ############################################################################
 
-## [[to do]] -- Plan to move to package:
-## * move code to separate directory (done)
-## * Move functions to separate file (done)
-## * make R project (r package)
-## * add git repo
-
 ## [[to do]]
-## * make this an R package!
-## * Unit tests
-## * Parameter checks
 ## * Make shiny app and host it on shiny.io (+ grab file from dropbox via R?) or on azure?
-
-## [[to do]]
-## * document and comment functions below!
-## * make bodyfat overall column
-## * flag bodyfat
-## * plot bodyfat
-## * extract common parts of plots (events, time axes, etc.)
-
-## [[to do]]
-## * function to get individual measurements from caliper measurements
-## * check for [[?]]
-
-## ============================================================================
-## build package tskeyvalparser
-## ============================================================================
-
-## [[to do]] move to helper code file in tskeyvalparser project
-
-# devtools::setup("/Users/ingonader/Dropbox/lists/health-parse/tskeyvalparser")
-# usethis::use_mit_license("Ingo Nader")
-# usethis::use_package("magrittr", "Suggests")
-# usethis::use_package("magrittr", "Depends")
-# usethis::use_package("magrittr", "Imports")  ## maybe use dplyr %>% instead?
-# usethis::use_package("readr")  ## Imports
-# usethis::use_package("purrr")
-# usethis::use_package("stringr")
-# usethis::use_package("lubridate")
-# usethis::use_package("tibble")
-#usethis::use_package("", "Suggests")
-
-# ## generate package level documentation:
-# usethis::use_package_doc()
-# ## add this to  package level description R file:
-# ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-# if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
-
-# rstudioapi::restartSession()
-
-# usethis::use_test("all")
-#usethis::use_roxygen_md()
-devtools::document()
-devtools::test()
-devtools::check()
-devtools::build()
-devtools::install()
-
 
 ## ============================================================================
 ## load packages
